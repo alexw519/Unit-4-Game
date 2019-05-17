17abcb2369f413d39c4f5dc3a796d4fafc78d6ef
Alex Wilson <alexw519@gmail.com>
2019-05-16 21:25:21 -0500

    Coded In The Other Characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,21 +5,79 @@ var obiWan =
     counterAttack: 30,
     health: 150
 }
+var lukeSky =
+{
+    charName: "Luke Skywalker",
+    attackPower: 8,
+    counterAttack: 30,
+    health: 120
+}
+var darthSid =
+{
+    charName: "Darth Sidous",
+    attackPower: 8,
+    counterAttack: 30,
+    health: 130
+}
+var darthMaul =
+{
+    charName: "Darth Maul",
+    attackPower: 8,
+    counterAttack: 30,
+    health: 200
+}
+
 var attackerPower;
 var defenderCounter;
 var characterSelected = false;
 var enemySelected = false;
 var obiWanCard = $("#obiCard");
+var lukeSkyCard = $("#lukeCard");
+var darthSidCard = $("#sidCard");
+var darthMaulCard = $("#maulCard");
 
 //Click Function For The Obi-Wan Card
 $("#obiCard").on("click", function()
 {
-    var clicked = false;
-    if (!clicked)
-    {
+    // var clicked = false;
+    // if (!clicked)
+    // {
         selection(obiWan, obiWanCard);
-        clicked = true;
-    }
+    //     clicked = true;
+    // }
+})
+
+//Click Function For The Luke Skywalker Card
+$("#lukeCard").on("click", function()
+{
+    // var clicked = false;
+    // if (!clicked)
+    // {
+        selection(lukeSky, lukeSkyCard);
+    //     clicked = true;
+    // }
+})
+
+//Click Function For The Darth Sidous Card
+$("#sidCard").on("click", function()
+{
+    // var clicked = false;
+    // if (!clicked)
+    // {
+        selection(darthSid, darthSidCard);
+    //     clicked = true;
+    // }
+})
+
+//Click Function For The Darth Maul Card
+$("#maulCard").on("click", function()
+{
+    // var clicked = false;
+    // if (!clicked)
+    // {
+        selection(darthMaul, darthMaulCard);
+    //     clicked = true;
+    // }
 })
 
 //Moves The Card Selected
@@ -31,11 +89,13 @@ function selection(character, cardName)
         attackerPower = character.attackPower;
         $("#yourChar").append(cardName);
     }
-    if (characterSelected && !enemySelected)
+
+    else if (characterSelected && !enemySelected)
     {
         enemySelected = true;
         defenderCounter = character.counterAttack;
         $("#defender").append(cardName);
         //Show The Attack Button
     }
+
 }
